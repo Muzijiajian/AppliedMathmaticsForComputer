b94ef6d08bb5bb8d657cab00c124b471753f8e12
Muzijiajian <caffeeli@icloud.com>
2016-03-18 22:15:18 +0800

    hw1Finished
    可视化最终结果，被虐的比较惨

--- hw1.py
+++ hw1.py
@@ -22,8 +22,7 @@ def curve_fit_weight(train_data, train_label, degree, reg=0):
     weights = LA.inv(data*data.T + reg*np.eye(degree+1)) * data * np.matrix(train_label).T
     return weights.A1
 
-numpoints = 10
-numPloy = 3
+numpoints = 9   #数据点的个数
 # 绘制坐标轴定义
 fig = plt.figure(num=1)
 plt.xlabel('x')
@@ -35,20 +34,21 @@ x1 = np.linspace(0, 1, 1e4)
 y1 = np.sin(2 * x1 * np.pi)
 plt.plot(x1, y1, color='green', label='Sin(x)', linewidth=2)
 # 绘制原始数据点
-x2 = np.linspace(0, 1, 10)  #np.random.normal(size=numpoints)
+x2 = np.linspace(0, 1, numpoints)  #np.random.normal(size=numpoints)
 noise1 = norm.rvs(0, size=numpoints, scale=0.15)
 noise2 = np.random.normal(0, 0.2, size=numpoints)
 y2 = np.sin(x2 * 2*np.pi) + noise1
 plt.plot(x2, y2, 'o', label='data with noise', mew=2, mec='b', mfc='none', ms=6)
 # 绘制多项式逼近曲线
-Reg = np.exp(-17)
-degree = 3
+Reg = np.exp(-18)
+degree = 9
 weight = curve_fit_weight(x2, y2, degree, Reg)
 print 'Fitting Parameters:', weight
 po = np.poly1d(weight)          # 产生一个参数为weight的多项式
 new_x = np.linspace(0, 1, 1e4)
-plt.plot(new_x, po(new_x), '-r', label='Poly Fitting Line(degree=3)', linewidth=2)
-# 绘制后保存图片
-plt.legend()
+plt.plot(new_x, po(new_x), '-r', label='Poly Fitting Line(degree=9, lnR=e-18)', linewidth=2)
+# 绘制图例说明,图例显示在图表的上方
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.03), fancybox=True, ncol=3)
+# 绘制后保存图
 plt.show()
 fig.savefig('poly_degree_9.png')
